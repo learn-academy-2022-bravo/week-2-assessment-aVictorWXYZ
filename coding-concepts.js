0219d1dbfd8b1914851c449d49d94ee55f185271
aVictorWXYZ <victorargote@gmail.com>
2022-04-02 18:10:41 -0700

    coding-concepts done

--- coding-concepts.js
+++ coding-concepts.js
@@ -8,10 +8,10 @@
 // --------------------1) What will this log?
 
 var cohort = "Bravo 2022"
-// console.log(cohort.split(""))
+console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:It will have each character in seperate index of an array 
+// b) Verify and explain: correct, each individual character to include space is in its own index of an array
 
 
 // --------------------2) What will this log?
@@ -19,28 +19,28 @@ var cohort = "Bravo 2022"
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:Hello, Learn Student
+// b) Verify and explain: incorrect, it didnt have a return so the function dosn't return anything 
 
 
 // --------------------3) What will this log?
 
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
-// console.log(multBy2)
+console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [8, 10, 12, 14, 16]
+// b) Verify and explain: correct, the map function goes to each index and multiplies it by 2
 
 
 // --------------------4) What will this log?
 
 var onlyOdds = [11, 12, 13, 14, 15].filter(value => value % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:[11, 13, 15]
+// b) Verify and explain: correct, the .filter dosn't keep any values that are not true after passing the evaluation at the end
 
 
 // --------------------5) What will this log?
@@ -51,10 +51,10 @@ var myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:JavaScript
+// b) Verify and explain: correct, the function follows the path down to the answer of 0 index of languages in myCodingSkills
 
 
 // --------------------STRETCH: What will this log?
@@ -67,7 +67,10 @@ class Learn {
   }
 }
 var learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:    
+    this.student = "George",
+    this.cohort = "Bravo",
+    this.year = 2022
+// b) Verify and explain: correct, the new class has the george take place of name where it was left to autofill with the value given
